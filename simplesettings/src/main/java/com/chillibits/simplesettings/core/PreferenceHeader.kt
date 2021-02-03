859bc81e906c953c549aeafb252e93d3da7d5c7a
Marc Auberer <marc.auberer@chillibits.com>
2021-02-03 23:51:38 +0100

    Add JavaDoc for new classes

--- simplesettings/src/main/java/com/chillibits/simplesettings/core/PreferenceHeader.kt
+++ simplesettings/src/main/java/com/chillibits/simplesettings/core/PreferenceHeader.kt
@@ -3,6 +3,10 @@ package com.chillibits.simplesettings.core
 import android.content.Context
 import androidx.annotation.LayoutRes
 
+/**
+ * Page element for page headers / setting screen headers.
+ * More information: https://github.com/marcauberer/simple-settings/wiki/PreferenceHeader
+ */
 class PreferenceHeader(
         context: Context,
         iconSpaceReservedByDefault: Boolean
